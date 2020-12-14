7b24cdce72c9a88ea753312d4f1780526b45b70c
knjhw7 <72421983+knjhw7@users.noreply.github.com>
2020-12-14 09:29:47 +0900

    1214

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.goods_list .goods_img').hover (
+    $('.overeffect').hover (
         function () {
             var _img = $(this).children().attr('src');
             _img = _img.replace('.png', '_on.png');
