eb307b5825b81f61685f2b93859756ec69153a69
knjhw07 <knjhw07@gmail.com>
2020-12-16 19:13:26 +0900

    1216

--- js/common.js
+++ js/common.js
@@ -70,5 +70,58 @@ $(document).ready(function () {
       if (e.keyCode === 27) _closeBtn.click();
     });
   });
+// 모바일 전체메뉴 모달
+  //열기 버튼 클릭 이벤트
+  $('.btn_allmenu').on('click', function () {
+    //1) 필요한 변수 선언
+    var _openBtn = $(this); //모달 닫기 버튼 클릭시 포커스 강제 이동을 위해
+    var _mdCnt = $('.allmenu_wrap');
+    var _closeBtn = _mdCnt.find('.close_btn');
+    var _first = _mdCnt.find('.first');
+    var _last = _mdCnt.find('.last');
+    var timer = 0;
+
+    //2) 스크린리더에서는 열려진 모달 말고는 접근하지 못하도록 제어(보조기술이 미구현 되어서 추가해 줌) aria-hidden="true" inert(비활성, 불활성)
+    _mdCnt.siblings().attr({'aria-hidden': true, inert: true});
+
+    //5) 위치 제어가 끝나면 #dim, 모달 컨텐츠를 보여지게 처리, 첫번째 링크에 포커스 강제 이동
+    _mdCnt.css('visibility', 'visible').stop().animate({left:0});
+    _first.focus();
+
+    // 6) 접근성을 위해 추가 : 닫기 버튼을 누르기 전까지 포커스는 모달 내부에 존재해야 함
+    //첫번째 링크에서 shift+tab을 누르면 가장 마지막으로 포커스 강제이동
+    _first.on('keydown', function (e) {
+      //console.log(e.keyCode); //tab 9
+      if (e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();   //이전으로 되돌아가는 기본 기능 차단
+        _last.focus();
+      }
+    });
+
+    //마지막 링크에서 shift(X)+tab을 누르면 가장 처음으로 포커스 강제이동
+    _last.on('keydown', function (e) {
+      if (!e.shiftKey && e.keyCode === 9) {
+          e.preventDefault(); //기본 기능차단
+          _first.focus();     //포커스 처음으로
+      }
+    });
+
+    //닫기 버튼 클릭 이벤트
+    _closeBtn.on('click', function () {
+      _mdCnt.stop().animate({left:'-100%'}, function (){
+        $(this).css('visibility', 'visible');
+        _mdCnt.siblings().removeAttr('aria-hidden inert');
+      });
+
+      //3) 열기 버튼으로 포커스 강제 이동
+      _openBtn.focus();
+    });
+
+    //esc 키보드를 누른 경우도 닫겨진다
+    $(window).on('keydown', function (e) {
+      //console.log(e.keyCode); //esc 27
+      if (e.keyCode === 27) _closeBtn.click();
+    });
+  });
 
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
     $('.overeffect').hover (
         function () {
@@ -21,21 +20,44 @@ $(document).ready(function() {
         if (scrollY >= $(document).height() - $(this).height() - $('#footer').outerHeight()) $('.floating_bar').addClass('btm');
         else $('.floating_bar').removeClass('btm');
     });
-=======
-$(document).ready(function() {
-    $('.overeffect').hover (
-        function () {
-            var _img = $(this).children().attr('src');
-            _img = _img.replace('.png', '_on.png');
-            console.log(_img);
-            $(this).children().attr({src:_img});
+
+    var promotionSwiper = new Swiper('.promotion .swiper-container', {
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
         },
-        function () {
-            var _img = $(this).children().attr('src');
-            _img = _img.replace('_on.png', '.png');
-            console.log(_img);
-            $(this).children().attr({src:_img});
-        }
-    );
->>>>>>> 7b24cdce72c9a88ea753312d4f1780526b45b70c
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },   
+        autoplay: {
+            // delay: 1000,
+          },
+      })
+       //play, stop
+  //일시정지 클릭
+  $('.promotion .controller .autostop').on('click', function () {
+    promotionSwiper.autoplay.stop(); //https://swiperjs.com/api/#autoplay
+    return false;
+  });
+  //자동실행 클릭
+  $('.promotion .controller .autoplay').on('click', function () {
+    promotionSwiper.autoplay.start();
+    return false;
+  });
+  $(window).on('scroll', function () {
+    var scrollY = $(this).scrollTop();
+    console.log(scrollY);
+    var bannerHei = $('.promotion').height();
+    console.log(bannerHei);
+    if (scrollY > bannerHei) {
+        $('#wrap').addClass('on').find('.header').addClass('fix')
+    } else {
+        $('#wrap').removeClass('on').find('.header').removeClass('fix')
+    }
+  });
 });
